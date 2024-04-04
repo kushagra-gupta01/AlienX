96a17894e87fde09a3b352e0fabf5a689d497ea8
Kushagra-Gupta01 <kushagra.gupta0077@gmail.com>
2024-04-05 01:08:54 +0530

    just added two routes to ensure correct working

--- app/main.go
+++ app/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"github.com/kushagra-gupta01/alienx"
+	"github.com/kushagra-gupta01/AlienX"
+	"github.com/kushagra-gupta01/AlienX/app/view/profile"
+	"github.com/kushagra-gupta01/AlienX/app/view/dashboard"
 )
 
 func main() {
 	app := AlienX.New()
-	app.Get("/profile", HandleUserProfile)
+	app.Get("/profile", HandleProfileIndex)
+	app.Get("/dashboard", HandleDashboardIndex)
 	app.Start(":3000")
 }
 
-func HandleUserProfile(c *AlienX.Context) error {
-	return c.Render(profile.Index())
+func HandleProfileIndex(c *AlienX.Context) error {
+	user := profile.User{
+		FirstName: "kk",
+		LastName:  "g",
+		Email:     "kk@kk.in",
+	}
+	return c.Render(profile.Index(user))
+}
+
+func HandleDashboardIndex(c *AlienX.Context) error {
+	return c.Render(dashboard.Index())
 }
